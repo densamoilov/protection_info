db8b0285088b8b548b31587bb3ce9e73ce2a0aa3
Denis Samoylov <samoylov.denis.i@gmail.com>
2015-10-01 22:39:55 +0600

    lab2

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -7,6 +7,7 @@ typedef long long llong;
 
 int main()
 {
+/*---------------------- Shamir --------------------
     llong m = 1234; // message
 
     std::default_random_engine gen;
@@ -63,6 +64,59 @@ int main()
     std::cout << "x3 = " << x3 << std::endl;
     std::cout << "x4 = " << x4 << std::endl;
     std::cout << "m = " << m << std::endl;
+--------------------------------------------------------*/
+
+/*--------------------------- El gamal -------------------*/
+    llong p{0}, d{0}, m{1234};
+    srand(time(0));
+    bool isprime = false;
+    // Generation a prime number
+    while (!isprime) {
+        isprime = true;
+        p = rand() % 30000;
+        // Check for prime
+        for (size_t i = 2; i <= static_cast<size_t>(sqrt(p)); ++i) {
+            if (p % i == 0) {
+                 isprime = false;
+                 break;
+            }
+        }
+        // if m is prime, check d
+        if (isprime) {
+            d = (p - 1) / 2;
+            for (size_t i = 2; i <= static_cast<size_t>(sqrt(d)); ++i) {
+                if (d % i == 0) {
+                    isprime = false;
+                    break;
+                }
+            }
+        }
+    }
+    llong g = 2;
+    while (pow_module(g, d, p) == 1) {
+        ++g;
+    }
+    std::default_random_engine genn;
+    std::uniform_int_distribution<llong> randomm(2, p - 2);
+    // Secret number
+    llong Ca = randomm(genn);
+    llong Cb = randomm(genn);
+
+    llong Da = pow_module(g, Ca, p);
+    llong Db = pow_module(g, Cb, p);
+
+    std::default_random_engine trans;
+    std::uniform_int_distribution<llong> randtr(1, p - 2);
+
+    llong k = randtr(trans);
+
+    llong r = pow_module(g, k, p);
+    llong e = m * pow_module(Db, k, p);
+
+    llong mrecd = e * pow_module(r, p - 1 - Cb, p);
+
+    std::cout << "Sent message: " << m << std::endl;
+    std::cout << "Received message: " << mrecd << std::endl;
 
     return 0;
 }
